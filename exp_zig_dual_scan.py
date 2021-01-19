7df048e6b123f05e8da3b5cccbf341de821fed64
John Mikulskis <jkulskis@bu.edu>
2021-01-19 14:38:04 -0500

    iterations from arg

--- exp_zig_dual_scan.py
+++ exp_zig_dual_scan.py
@@ -100,19 +100,23 @@ if __name__ == '__main__':
     channel = 11
     START=11
     STEP=1
-    STOP=26
+    STOP=18
     STEPS = [x for x in range(START,STOP+STEP,STEP)]
+
+    iterations = int(sys.argv[1]) if len(sys.argv) == 2 else 1
+    print(f"Num iterations: {iterations}")
 
     print(STEPS)
     print("channel "+str(channel))
-    subp = Popen(['python3','grc-flowgraphs/zigbee_dual_channel_scan.py']) #, stdout=PIPE, stderr=PIPE)
+    subp = Popen(['python3','zigbee_dual_channel_scan.py']) #, stdout=PIPE, stderr=PIPE)
     time.sleep(6)
 
-    for vary in STEPS:
-        print("CURRENT STEP "+str(vary))
-        gnuradio_set_vars(channel=vary)
-        print('Freq:', gnuradio_get_vars('channel'))
-        time.sleep(5)
+    for _ in range(iterations):
+        for vary in STEPS:
+            print("CURRENT STEP "+str(vary))
+            gnuradio_set_vars(channel=vary)
+            print('Freq:', gnuradio_get_vars('channel'))
+            time.sleep(5)
 
     
     #time.sleep(10)
